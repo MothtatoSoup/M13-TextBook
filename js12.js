4105ccdce1b78d861874bee9bfb56092e1bcb6b7
MothtatoSoup <maddy.puryear@outlook.com>
2025-04-25 13:28:00 -0400

    page 515

--- js12.js
+++ js12.js
@@ -11,3 +11,23 @@
     Filename: js12.js
 */
 
+// run once the page is loaded and ready
+$ ( () => {
+
+    // Add cllick events to each question in the FAQ
+    $("dl#faq dt").click( e => {
+
+        // Alt btwn hiding and showing the answer
+        let question = $(e.target);
+        let answer = $(question.next());
+
+        $(question).toggleClass("hiddenAnswer");
+
+        if ($(question).hasClass("hiddenAnswer")) {
+            $(answer).hide();
+        } else {
+            $(answer).show();
+        }
+    });
+
+});
